a1760fc6e1b575155f0ffb1d8b9c9fa3875e30e6
Alan Borowy <borowyalan@gmail.com>
2019-10-11 16:12:32 +0200

    add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
+import Footer from "./Footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -27,9 +28,7 @@ const Layout = ({ children }) => {
 		<div className="wrapper">
 			<Header siteTitle={data.site.siteMetadata.title} />
 			<main>{children}</main>
-			<footer>
-				<div>footer</div>
-			</footer>
+			<Footer />
 		</div>
 	)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import Hero from "../components/Hero"
-import About from "../components/About"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+import Hero from "../components/Hero";
+import About from "../components/About";
 
 const IndexPage = () => (
 	<Layout>
-		<SEO title="Home" />
+		<SEO title='Home' />
 		<Hero></Hero>
 		<About></About>
 	</Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
