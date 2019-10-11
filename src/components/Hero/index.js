7669d2ca530b4ef1a2aea832543cfdd823acad69
Alan Borowy <borowyalan@gmail.com>
2019-10-11 08:32:20 +0200

    add example text to index page

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,19 +3,19 @@ import "./hero.css"
 
 export default function index() {
 	return (
-		<section className="section">
+		<section id="hero" className="section">
 			<div className="container">
 				<div className="columns">
 					<div className="column is-half">
 						<div className="hero is-medium">
 							<div className="hero-body">
 								<h1 className="is-size-1 is-size-3-mobile">
-									Porozmawiajmy o aktywności szkolnej i edukacji
+									Porozmawiajmy o aktywności szkolnej i edukacji.
 								</h1>
-								<h2 className="is-size-4-mobile">
-									Lorem ipsum dolor sit amet consectetur.
-								</h2>
-								<button className="heroButton">Dowiedz się więcej</button>
+								<h2 className="is-size-4-mobile">Konferencja samorządności.</h2>
+								<button className="button is-primary heroButton">
+									Dowiedz się więcej
+								</button>
 							</div>
 						</div>
 					</div>
