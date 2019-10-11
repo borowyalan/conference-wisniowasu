ee4c50da984c16e49f3c52a0060e3a3f4135e55b
Alan Borowy <borowyalan@gmail.com>
2019-10-11 15:50:39 +0200

    add proper icons

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,57 +1,34 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCoffee } from "@fortawesome/free-solid-svg-icons"
 
 import "../../pages/mystyles.scss"
-import "./about.css"
+import "./about.scss"
+import {
+	FaBeer,
+	FaGraduationCap,
+	FaBookReader,
+	FaLightbulb,
+} from "react-icons/fa"
 
 export default function About() {
 	return (
 		<section className="section" id="about">
 			<div className="container">
-				<h2 className={`title is- has-text-centered`}>About</h2>
+				<h2 className={`title is- has-text-centered`}>O nas:</h2>
 				<div className="columns is-multiline">
 					<div className="column is-half">
 						<div className="box">
 							<article className="media">
 								<div className="media-left">
-									<div className="icon">icon</div>
+									<FontAwesomeIcon icon={faCoffee} />
 								</div>
 								<div className="media-content">
 									<div className="content">
-										<h3 className="title is-3 is-size-4-mobile">Title</h3>
-										<p className="is-size-5 is-size-6-mobile">subtitle</p>
-									</div>
-								</div>
-							</article>
-						</div>
-					</div>
-					<div className="column is-half">
-						<div className="box">
-							<article className="media">
-								<div className="media-left">
-									<div className="icon">icon</div>
-								</div>
-								<div className="media-content">
-									<div className="content">
-										<h3 className="title is-3 is-size-4-mobile">Title</h3>
-										<p className="is-size-5 is-size-6-mobile">subtitle</p>
-									</div>
-								</div>
-							</article>
-						</div>
-					</div>
-					<div className="column is-half">
-						<div className="box">
-							<article className="media">
-								<div className="media-left">
-									<div className="icon">icon</div>
-								</div>
-								<div className="media-content">
-									<div className="content">
-										<h3 className="title is-3 is-size-4-mobile">
-											Title lorem lorem lorem
-										</h3>
+										<h3 className="title is-3 is-size-4-mobile">Lorem ipsum</h3>
 										<p className="is-size-5 is-size-6-mobile">
-											subtitle lorem lorem lorem
+											Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											Tempore, eius?
 										</p>
 									</div>
 								</div>
@@ -62,12 +39,51 @@ export default function About() {
 						<div className="box">
 							<article className="media">
 								<div className="media-left">
-									<div className="icon">icon</div>
+									<FaGraduationCap size="1em" className="fa-fw" />
 								</div>
 								<div className="media-content">
 									<div className="content">
-										<h3 className="title is-3 is-size-4-mobile">Title</h3>
-										<p className="is-size-5 is-size-6-mobile">subtitle</p>
+										<h3 className="title is-3 is-size-4-mobile">Lorem ipsum</h3>
+										<p className="is-size-5 is-size-6-mobile">
+											Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											Tempore, eius?
+										</p>
+									</div>
+								</div>
+							</article>
+						</div>
+					</div>
+					<div className="column is-half">
+						<div className="box">
+							<article className="media">
+								<div className={`media-left fa-fw`}>
+									<FaBeer className="fa-fw" />
+								</div>
+								<div className="media-content">
+									<div className="content">
+										<h3 className="title is-3 is-size-4-mobile">Lorem ipsum</h3>
+										<p className="is-size-5 is-size-6-mobile">
+											Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											Tempore, eius?
+										</p>
+									</div>
+								</div>
+							</article>
+						</div>
+					</div>
+					<div className="column is-half">
+						<div className="box">
+							<article className="media">
+								<div className={`media-left fa-fw`}>
+									<FaLightbulb size="1em" className="fa-fw" />
+								</div>
+								<div className="media-content">
+									<div className="content">
+										<h3 className="title is-3 is-size-4-mobile">Lorem ipsum</h3>
+										<p className="is-size-5 is-size-6-mobile">
+											Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											Tempore, eius?
+										</p>
 									</div>
 								</div>
 							</article>
