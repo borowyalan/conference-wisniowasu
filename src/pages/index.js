f6735291b79cd474f4908b70f7b4a4ebc1a9ee24
Alan Borowy <borowyalan@gmail.com>
2019-10-11 16:56:22 +0200

    move landing page components to separate folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from "react"
+import { Link } from "gatsby"
 
-import Layout from "../components/layout";
-import Image from "../components/image";
-import SEO from "../components/seo";
-import Hero from "../components/Hero";
-import About from "../components/About";
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import Hero from "../components/landing/Hero"
+import About from "../components/landing/About"
 
 const IndexPage = () => (
 	<Layout>
-		<SEO title='Home' />
+		<SEO title="Konferencja samorządności" />
 		<Hero></Hero>
 		<About></About>
 	</Layout>
-);
+)
 
-export default IndexPage;
+export default IndexPage
--- src/pages/o-nas.js
+++ src/pages/o-nas.js
@@ -5,9 +5,18 @@ import SEO from "../components/seo"
 
 const AboutPage = () => (
 	<Layout>
-		<SEO title="About" />
-		<h1>Hi from the about page</h1>
-		<p>Welcome to about</p>
+		<SEO title="O nas" />
+		<div className="section">
+			<div className="container mainWrapper">
+				<div className="container contentWrapper">
+					<h1>Hi from the about page</h1>
+					<p>Welcome to about</p>
+				</div>
+				<div className="columns">
+					<div className="column is-half">sdf</div>
+				</div>
+			</div>
+		</div>
 	</Layout>
 )
 
