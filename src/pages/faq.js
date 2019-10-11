7e06b7f53cf8f87da66619c3f28344fc65ed1d04
Alan Borowy <borowyalan@gmail.com>
2019-10-11 08:32:01 +0200

    add about and faq pages drafts

---
+++ src/pages/faq.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const FAQPage = () => (
+	<Layout>
+		<SEO title="Frequenly Asked Questions" />
+		<section className="section">
+			<div className="container">
+				<h2 className="title has-text-centered">Hi from the FAQ</h2>
+				<p>Welcome to FAQ</p>
+			</div>
+		</section>
+	</Layout>
+)
+
+export default FAQPage
