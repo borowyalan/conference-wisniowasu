a2c6c85fc4752d4ee130a7394a9dd082545d1071
Alan Borowy <borowyalan@gmail.com>
2019-10-28 20:21:53 +0100

    change button to gatsby Link & format

--- src/components/landing/Hero/index.js
+++ src/components/landing/Hero/index.js
@@ -1,26 +1,30 @@
-import React from "react";
-import "./hero.scss";
+import React from "react"
+import "./hero.scss"
+import { Link } from "gatsby"
 
 export default function index() {
 	return (
-		<section id='hero' className='section'>
-			<div className='container'>
-				<div className='columns'>
-					<div className='column is-half'>
-						<div className='hero is-medium'>
-							<div className='hero-body'>
-								<h1 className='is-size-1 is-size-3-mobile'>
+		<section id="hero" className="section">
+			<div className="container">
+				<div className="columns">
+					<div className="column is-half">
+						<div className="hero is-medium">
+							<div className="hero-body">
+								<h1 className="is-size-1 is-size-2-mobile">
 									Porozmawiajmy o&nbsp;aktywności szkolnej i&nbsp;edukacji.
 								</h1>
-								<h2 className='is-size-4-mobile'>Konferencja samorządności.</h2>
-								<button className='button is-primary is-large heroButton'>
+								<h2 className="is-size-4-mobile">Konferencja samorządności.</h2>
+								<Link
+									className="button is-primary is-large heroButton"
+									to="/o-nas"
+								>
 									Dowiedz się więcej
-								</button>
+								</Link>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</section>
-	);
+	)
 }
