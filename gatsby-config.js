5575d73862f1233436bc5b58d771eaa911e4da58
Alan Borowy <borowyalan@gmail.com>
2020-01-16 15:46:38 +0100

    meta: change site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	siteMetadata: {
-		title: ``,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		title: `Konferencja Samorządności | Wiśniowa`,
+		description: 'Sprawdź co możemy zrobic wspólnymi siłamy na konferencji samorządnośći na Wiśniowej',
 		author: `@Borowy Alan`,
 	},
 	plugins: [
