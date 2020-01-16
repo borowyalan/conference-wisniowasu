179cd53eb64bb3fca76c461c2186e9ca7ca789b7
Alan Borowy <borowyalan@gmail.com>
2020-01-16 17:52:07 +0100

    make timestamp stand out

--- src/pages/program.js
+++ src/pages/program.js
@@ -18,88 +18,98 @@ const program = () => (
 							<h1>Program konferencji Samorządów Warszawskich P.ASJE</h1>
 							<h4>(Porozmawiajmy o Aktywności Społecznej i Edukacji) :</h4>
 							<ul>
-								<li>
+								<li className="time">
 									<strong>9.00-9.30</strong> – Przywitanie gości, oficjalne
 									rozpoczęcie konferencji
 								</li>
-								<li>
+								<li className="time">
 									<strong>9.40-10.45</strong> – Panele dyskusyjne:
 									<ol>
 										<li>
-											<strong>
-												“Szkoła przyszłości”
-											</strong> - jak widzimy szkołę za X lat? Jakie
-											formy i metody pracy można stosować, by uatrakcyjnić
-											proces nauczania? Jakie są inne warianty edukacji, których
-											do tej pory nie braliśmy pod uwagę? (Liceum w Chmurze).
+											<strong>“Szkoła przyszłości”</strong> - jak widzimy szkołę
+											za X lat? Jakie formy i metody pracy można stosować, by
+											uatrakcyjnić proces nauczania? Jakie są inne warianty
+											edukacji, których do tej pory nie braliśmy pod uwagę?
+											(Liceum w Chmurze).
 										</li>
 										<li>
-											<strong>“Kryzys klimatyczny”</strong> - z jakimi problemami przyjdzie się
-											zmierzyć światu i nam – obywatelom? co jako uczniowie
-											możemy zrobić, aby spowalniać proces zmian klimatycznych?
-											(we współpracy z Młodzieżowym Strajkiem Klimatycznym).
+											<strong>“Kryzys klimatyczny”</strong> - z jakimi
+											problemami przyjdzie się zmierzyć światu i nam –
+											obywatelom? co jako uczniowie możemy zrobić, aby
+											spowalniać proces zmian klimatycznych? (we współpracy z
+											Młodzieżowym Strajkiem Klimatycznym).
 										</li>
 										<li>
-											<strong>„Internet trzepakiem XXI wieku”</strong> – czyli jaki wpływ na
-											człowieka ma wypieranie spotkań towarzyskich przez media
-											społecznościowe i w jaki sposób ewoluują media (fundacja
-											Nowy Głos).
+											<strong>„Internet trzepakiem XXI wieku”</strong> – czyli
+											jaki wpływ na człowieka ma wypieranie spotkań towarzyskich
+											przez media społecznościowe i w jaki sposób ewoluują media
+											(fundacja Nowy Głos).
 										</li>
 										<li>
-											<strong>Szkoła Impro</strong> - zajęcia dla niezdecydowanych, metody
-											aktywizacji poprzez improwizację i sztukę.
+											<strong>Szkoła Impro</strong> - zajęcia dla
+											niezdecydowanych, metody aktywizacji poprzez improwizację
+											i sztukę.
 										</li>
 									</ol>
 								</li>
-								<li>
+								<li className="time">
 									<strong>10.50-11.20</strong> – Przerwa kawowa
 								</li>
-								<li>
+								<li className="time">
 									<strong>11.25-12.55</strong> – Warsztaty:
 								</li>
 								<ol>
 									<li>
-										<strong>Jak być liderem, a może przywódcą zespołu współpracowników?</strong>
+										<strong>
+											Jak być liderem, a może przywódcą zespołu
+											współpracowników?
+										</strong>
 									</li>
-									<li><strong>Wykorzystywanie mediów społecznościowych w pracy SU.</strong></li>
+									<li>
+										<strong>
+											Wykorzystywanie mediów społecznościowych w pracy SU.
+										</strong>
+									</li>
 									<li>
 										<strong>
 											Wybory na przewodniczącego SU i prowadzenie kampanii
 											wyborczej
-										</strong> - przykłady kampanii wyborczych, jak powinna
-										wyglądać, jakich błędów unikać, na jakie niebezpieczeństwa
-										trzeba być gotowym, jak dobrze przygotować się do kampanii
-										(sekcja prawna SU we współpracy z fundacją Civis Polonus).
+										</strong>{" "}
+										- przykłady kampanii wyborczych, jak powinna wyglądać,
+										jakich błędów unikać, na jakie niebezpieczeństwa trzeba być
+										gotowym, jak dobrze przygotować się do kampanii (sekcja
+										prawna SU we współpracy z fundacją Civis Polonus).
 									</li>
 									<li>
-										<strong>Technologia w służbie SU</strong>– narzędzia i metody pracy z
-										wykorzystaniem technik, z których na co dzień korzystamy w
-										życiu szkolnym (sekcja techniczna SU).
+										<strong>Technologia w służbie SU</strong>– narzędzia i
+										metody pracy z wykorzystaniem technik, z których na co dzień
+										korzystamy w życiu szkolnym (sekcja techniczna SU).
 									</li>
 									<li>
-										<strong>Organizacja pracy SU w szkole</strong> – jak sprawnie zarządzać
-										ludźmi tak, aby zwyczajnie wszystko działało (sekcja
-										organizacyjna SU we współpracy z Centrum Komunikacji
-										Społecznej).
+										<strong>Organizacja pracy SU w szkole</strong> – jak
+										sprawnie zarządzać ludźmi tak, aby zwyczajnie wszystko
+										działało (sekcja organizacyjna SU we współpracy z Centrum
+										Komunikacji Społecznej).
 									</li>
 									<li>
-										<strong>Krok pierwszy - jak rozkręcić SU?</strong> - pierwsze kroki, rozmowa
-										z publicznością na temat problemów w zarządzaniu samorządem,
-										jak budować relacje z dorosłymi - nauczycielami, dyrekcją,
-										rodzicami? (we współpracy z absolwentem szkoły)
+										<strong>Krok pierwszy - jak rozkręcić SU?</strong> -
+										pierwsze kroki, rozmowa z publicznością na temat problemów w
+										zarządzaniu samorządem, jak budować relacje z dorosłymi -
+										nauczycielami, dyrekcją, rodzicami? (we współpracy z
+										absolwentem szkoły)
 									</li>
 									<li>
-										<strong>Realne zmiany w szkole dzięki SU</strong> – wymiana doświadczeń
-										między samorządem z Wiśniowej a zaproszonymi
+										<strong>Realne zmiany w szkole dzięki SU</strong> – wymiana
+										doświadczeń między samorządem z Wiśniowej a zaproszonymi
 										przedstawicielami samorządów innych szkół warszawskich,
 										przestrzeń do dyskusji dla młodzieży (nasze osiągnięcia).
 									</li>
 									<li>
-										<strong>Warsztaty dla opiekunów SU</strong> (we współpracy z fundacją Civis
-										Polonus).
+										<strong>Warsztaty dla opiekunów SU</strong> (we współpracy z
+										fundacją Civis Polonus).
 									</li>
 								</ol>
-								<li>
+								<li className="time">
 									<strong>13.00</strong> – Podsumowanie konferencji
 								</li>
 							</ul>
