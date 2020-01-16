269770ccae35ed83dcc8c4c2785a56442e8f9e98
Alan Borowy <borowyalan@gmail.com>
2020-01-16 20:10:24 +0100

    add headers to form

--- src/components/landing/Form/index.js
+++ src/components/landing/Form/index.js
@@ -22,11 +22,16 @@ export default function index() {
 		form.title = e.target.title.value
 		form.message = e.target.message.value
 
+		console.log(JSON.stringify(form))
+
 		fetch("https://api.wisniowasu.pl/mailer/sendmail", {
 			method: "post",
+			headers: {
+				"Content-type": "application/json",
+			},
 			body: JSON.stringify(form),
 		})
-			// .then(json)
+			.then(json)
 			.then(data => console.log(data))
 	}
 
