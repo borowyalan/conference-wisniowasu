cc4c0d34005057f1ef5fa950bed779e89de46c58
Alan Borowy <borowyalan@gmail.com>
2020-01-16 16:41:29 +0100

    dedicate program tab to an actual progam of the conference

--- src/pages/o-nas.js
+++ src/pages/o-nas.js
@@ -9,49 +9,71 @@ import "./subpages.scss"
 const AboutPage = () => (
 	<LayoutSubpages>
 		<SEO title="O nas" />
-		{/* <div id="about" className="section"> */}
-			{/* <div className="container">
+		<div id="faq" className="section">
+			<div className="container">
 				<div className="coontainer titleContainer">
 					<div className="columns">
 						<div className="column is-three-fifths">
-							<h1>O nas</h1>
-							<p>
-								Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-								Officiis atque officia culpa natus voluptatibus? Alias illum
-								nihil eaque repellat impedit necessitatibus inventore explicabo
-								eveniet maxime quisquam molestiae officia.
-							</p>
+							<h1>Kim jesteśmy?</h1>
+							<div className="faq-section">
+							
+								{/* <h3>Kim jesteśmy?</h3> */}
+								<p>
+									Niektórzy znają się od dawna, niektórzy poznali się
+									przypadkiem dzięki projektowi, niektórzy są partnerami,
+									niektórzy przyjaciółmi. Lubimy gry, rysować, interesuje nas
+									psychologia, czy nurkowanie, jesteśmy aspirującymi przyszłymi
+									menedżerami, artystami, bądź obecnymi pilnymi uczniami.
+									Jesteśmy też braćmi, siostrami, synami, córkami i przede
+									wszystkim przyjaciółmi. To co nas łączy to pasja, która
+									realizujemy poprzez samorządność – chęć zmieniania świata
+									dookoła nas na lepsze. Wiemy jednak, że siła zmiany jest jej
+									zasięg, dlatego sami nie naprawimy tego świata. Jako uczniowie
+									technikum informatyczno-mechatronicznego znamy wartość nowych
+									technologii i możliwości, jakich dostarczają, ale mimo to
+									uważamy że nic nie zastąpi spotkania twarzą w twarz, dlatego
+									zdecydowaliśmy się zorganizować konferencję.
+								</p>
+							</div>
 
-							<div className="container contentContainer">
+							<h1>Co już zrobiliśmy?</h1>
+							<div className="faq-section">
+								<h3>Aula Czekoladowa i galeria uzdolnionych</h3>
 								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Cupiditate nostrum voluptas provident cum, repellendus aliquid
-									quam dolorum eum recusandae saepe rerum quasi odit quos
-									corrupti commodi? Quo quisquam quibusdam magni! Lorem ipsum
-									dolor, sit amet consectetur adipisicing elit. Ea eligendi
-									maxime sint officiis veritatis cupiditate ab deleniti veniam
-									deserunt cumque! Nesciunt cum ipsam, delectus culpa quam
-									aliquid ducimus id obcaecati?
+									4 lata temu jedna z naszych obecnych absolwentek wpadła na
+									pomysł aby na klatce schodowe odsłonić cegłę XIX-wiecznego
+									budynku i zrobić tam miejsce, dla wybitnych uczniów, aby można
+									było ich wyróżnić za wybitne osiągnięcia. Tak powstała galeria
+									uzdolnionych. Pomysł tak się spodobał, że poszliśmy za ciosem
+									i w następnym roku odnowiliśmy też aule naprzeciwko bufetu i
+									zrobiliśmy z niej miejsce ładne i przyjemne, do dyspozycji
+									uczniów i pracowników. Motyw cegieł stał się już w naszej
+									szkole klasyczny i przy remontach odkrywane są kolejne
+									fragmenty ceglanych murów.
 								</p>
+							</div>
+							<div className="faq-section">
+								<h3>WiFi</h3>
 								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Cupiditate nostrum voluptas provident cum, repellendus aliquid
-									quam dolorum eum recusandae saepe rerum quasi odit quos
-									corrupti commodi? Quo quisquam quibusdam magni! Lorem ipsum
-									dolor, sit amet consectetur adipisicing elit. Ea eligendi
-									maxime sint officiis veritatis cupiditate ab deleniti veniam
-									deserunt cumque! Nesciunt cum ipsam, delectus culpa quam
-									aliquid ducimus id obcaecati?
+									W związku z ubiegłorocznym sukcesem w rankingu perspektyw
+									wspólnie z dyrekcją postanowiliśmy uczcić to wydarzenie
+									wyjątkową imprezą. Zaproponowaliśmy dyrekcji coś w rodzaju
+									mini conventu z konkursem cosplayów, strefą konsol, loterią i
+									innymi atrakcjami. Wydarzenie tak się spodobało, że nawet
+									nauczyciele przebierali się i bawili wraz z uczniami. To był
+									dzień wspaniałej zabawy i wyjątkowej atmosfery w szkole.
 								</p>
+							</div>
+							<div className="faq-section">
+								<h3>Integracja klas pierwszych</h3>
 								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Cupiditate nostrum voluptas provident cum, repellendus aliquid
-									quam dolorum eum recusandae saepe rerum quasi odit quos
-									corrupti commodi? Quo quisquam quibusdam magni! Lorem ipsum
-									dolor, sit amet consectetur adipisicing elit. Ea eligendi
-									maxime sint officiis veritatis cupiditate ab deleniti veniam
-									deserunt cumque! Nesciunt cum ipsam, delectus culpa quam
-									aliquid ducimus id obcaecati?
+									Od 3 lat wspieramy coraz bardziej dyrekcję w integrowaniu
+									nowoprzybyłych uczniów. Organizujemy dla nich warsztaty pracy
+									zespołowej, zajęcia sportowe, oraz grę terenową w stylu
+									escape-room. Opinie które słyszymy na temat tej zabawy zwykle
+									są bardzo podobne. To świetne pierwsze wrażenie jakie robi
+									szkoła i idealna okazja żeby w miłej atmosferze poznać ludzi
+									ze swojej klasy, na co później często brakuje czasu.
 								</p>
 							</div>
 						</div>
@@ -59,45 +81,10 @@ const AboutPage = () => (
 				</div>
 
 				<div className="columns">
-					<div className="column is-three-fifths"></div>
-				</div>
-			</div> */}
-
-			<div id="faq" className="section">
-				<div className="container">
-					<div className="coontainer titleContainer">
-						<div className="columns">
-							<div className="column is-three-fifths">
-								<h1>Kim jesteśmy?</h1>
-								<div className="faq-section">
-									{/* <h3>Kim jesteśmy?</h3> */}
-									<p>
-										Niektórzy znają się od dawna, niektórzy poznali się
-										przypadkiem dzięki projektowi, niektórzy są partnerami,
-										niektórzy przyjaciółmi. Lubimy gry, rysować, interesuje nas
-										psychologia, czy nurkowanie, jesteśmy aspirującymi
-										przyszłymi menedżerami, artystami, bądź obecnymi pilnymi
-										uczniami. Jesteśmy też braćmi, siostrami, synami, córkami i
-										przede wszystkim przyjaciółmi. To co nas łączy to pasja,
-										która realizujemy poprzez samorządność – chęć zmieniania
-										świata dookoła nas na lepsze. Wiemy jednak, że siła zmiany
-										jest jej zasięg, dlatego sami nie naprawimy tego świata.
-										Jako uczniowie technikum informatyczno-mechatronicznego
-										znamy wartość nowych technologii i możliwości, jakich
-										dostarczają, ale mimo to uważamy że nic nie zastąpi
-										spotkania twarzą w twarz, dlatego zdecydowaliśmy się
-										zorganizować konferencję.
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<div className="columns">
-						<div className="column is-half"></div>
-					</div>
+					<div className="column is-half"></div>
 				</div>
 			</div>
+		</div>
 		{/* </div> */}
 	</LayoutSubpages>
 )
--- src/pages/program.js
+++ src/pages/program.js
@@ -5,55 +5,104 @@ import SEO from "../components/seo"
 
 import "./mystyles.scss"
 import "./subpages.scss"
+import "./program.scss"
 
 const program = () => (
 	<LayoutSubpages>
-		<SEO title="O nas" />
-		<div id="faq" className="section">
+		<SEO title="Program" />
+		<div id="program" className="section">
 			<div className="container">
 				<div className="coontainer titleContainer">
 					<div className="columns">
 						<div className="column is-three-fifths">
-							<h1>Co już zrobiliśmy?</h1>
-							<div className="faq-section">
-								<h3>Aula Czekoladowa i galeria uzdolnionych</h3>
-								<p>
-									4 lata temu jedna z naszych obecnych absolwentek wpadła na
-									pomysł aby na klatce schodowe odsłonić cegłę XIX-wiecznego
-									budynku i zrobić tam miejsce, dla wybitnych uczniów, aby można
-									było ich wyróżnić za wybitne osiągnięcia. Tak powstała galeria
-									uzdolnionych. Pomysł tak się spodobał, że poszliśmy za ciosem
-									i w następnym roku odnowiliśmy też aule naprzeciwko bufetu i
-									zrobiliśmy z niej miejsce ładne i przyjemne, do dyspozycji
-									uczniów i pracowników. Motyw cegieł stał się już w naszej
-									szkole klasyczny i przy remontach odkrywane są kolejne
-									fragmenty ceglanych murów.
-								</p>
-							</div>
-							<div className="faq-section">
-								<h3>WiFi</h3>
-								<p>
-									W związku z ubiegłorocznym sukcesem w rankingu perspektyw
-									wspólnie z dyrekcją postanowiliśmy uczcić to wydarzenie
-									wyjątkową imprezą. Zaproponowaliśmy dyrekcji coś w rodzaju
-									mini conventu z konkursem cosplayów, strefą konsol, loterią i
-									innymi atrakcjami. Wydarzenie tak się spodobało, że nawet
-									nauczyciele przebierali się i bawili wraz z uczniami. To był
-									dzień wspaniałej zabawy i wyjątkowej atmosfery w szkole.
-								</p>
-							</div>
-							<div className="faq-section">
-								<h3>Integracja klas pierwszych</h3>
-								<p>
-									Od 3 lat wspieramy coraz bardziej dyrekcję w integrowaniu
-									nowoprzybyłych uczniów. Organizujemy dla nich warsztaty pracy
-									zespołowej, zajęcia sportowe, oraz grę terenową w stylu
-									escape-room. Opinie które słyszymy na temat tej zabawy zwykle
-									są bardzo podobne. To świetne pierwsze wrażenie jakie robi
-									szkoła i idealna okazja żeby w miłej atmosferze poznać ludzi
-									ze swojej klasy, na co później często brakuje czasu.
-								</p>
-							</div>
+							<h1>Program konferencji Samorządów Warszawskich P.ASJE</h1>
+							<h4>(Porozmawiajmy o Aktywności Społecznej i Edukacji) :</h4>
+							<ul>
+								<li>
+									<strong>9.00-9.30</strong> – Przywitanie gości, oficjalne
+									rozpoczęcie konferencji
+								</li>
+								<li>
+									<strong>9.40-10.45</strong> – Panele dyskusyjne:
+									<ol>
+										<li>
+											<strong>
+												“Szkoła przyszłości”
+											</strong> - jak widzimy szkołę za X lat? Jakie
+											formy i metody pracy można stosować, by uatrakcyjnić
+											proces nauczania? Jakie są inne warianty edukacji, których
+											do tej pory nie braliśmy pod uwagę? (Liceum w Chmurze).
+										</li>
+										<li>
+											<strong>“Kryzys klimatyczny”</strong> - z jakimi problemami przyjdzie się
+											zmierzyć światu i nam – obywatelom? co jako uczniowie
+											możemy zrobić, aby spowalniać proces zmian klimatycznych?
+											(we współpracy z Młodzieżowym Strajkiem Klimatycznym).
+										</li>
+										<li>
+											<strong>„Internet trzepakiem XXI wieku”</strong> – czyli jaki wpływ na
+											człowieka ma wypieranie spotkań towarzyskich przez media
+											społecznościowe i w jaki sposób ewoluują media (fundacja
+											Nowy Głos).
+										</li>
+										<li>
+											<strong>Szkoła Impro</strong> - zajęcia dla niezdecydowanych, metody
+											aktywizacji poprzez improwizację i sztukę.
+										</li>
+									</ol>
+								</li>
+								<li>
+									<strong>10.50-11.20</strong> – Przerwa kawowa
+								</li>
+								<li>
+									<strong>11.25-12.55</strong> – Warsztaty:
+								</li>
+								<ol>
+									<li>
+										<strong>Jak być liderem, a może przywódcą zespołu współpracowników?</strong>
+									</li>
+									<li><strong>Wykorzystywanie mediów społecznościowych w pracy SU.</strong></li>
+									<li>
+										<strong>
+											Wybory na przewodniczącego SU i prowadzenie kampanii
+											wyborczej
+										</strong> - przykłady kampanii wyborczych, jak powinna
+										wyglądać, jakich błędów unikać, na jakie niebezpieczeństwa
+										trzeba być gotowym, jak dobrze przygotować się do kampanii
+										(sekcja prawna SU we współpracy z fundacją Civis Polonus).
+									</li>
+									<li>
+										<strong>Technologia w służbie SU</strong>– narzędzia i metody pracy z
+										wykorzystaniem technik, z których na co dzień korzystamy w
+										życiu szkolnym (sekcja techniczna SU).
+									</li>
+									<li>
+										<strong>Organizacja pracy SU w szkole</strong> – jak sprawnie zarządzać
+										ludźmi tak, aby zwyczajnie wszystko działało (sekcja
+										organizacyjna SU we współpracy z Centrum Komunikacji
+										Społecznej).
+									</li>
+									<li>
+										<strong>Krok pierwszy - jak rozkręcić SU?</strong> - pierwsze kroki, rozmowa
+										z publicznością na temat problemów w zarządzaniu samorządem,
+										jak budować relacje z dorosłymi - nauczycielami, dyrekcją,
+										rodzicami? (we współpracy z absolwentem szkoły)
+									</li>
+									<li>
+										<strong>Realne zmiany w szkole dzięki SU</strong> – wymiana doświadczeń
+										między samorządem z Wiśniowej a zaproszonymi
+										przedstawicielami samorządów innych szkół warszawskich,
+										przestrzeń do dyskusji dla młodzieży (nasze osiągnięcia).
+									</li>
+									<li>
+										<strong>Warsztaty dla opiekunów SU</strong> (we współpracy z fundacją Civis
+										Polonus).
+									</li>
+								</ol>
+								<li>
+									<strong>13.00</strong> – Podsumowanie konferencji
+								</li>
+							</ul>
 						</div>
 					</div>
 				</div>
