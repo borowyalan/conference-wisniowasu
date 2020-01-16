dc404b810d4d4035ac8a3daef9f5355e22561845
Alan Borowy <borowyalan@gmail.com>
2020-01-16 12:03:46 +0100

    add proper icons

--- src/components/Landing/About/index.js
+++ src/components/Landing/About/index.js
@@ -1,18 +1,11 @@
 import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCoffee } from "@fortawesome/free-solid-svg-icons"
+import handshake_icon from "../../../images/handshake.svg"
+import coffee_icon from "../../../images/coffee.svg"
+import friendship_icon from "../../../images/friendship.svg"
+import lightbulb_icon from "../../../images/lightbulb.svg"
 
 import "../../../pages/mystyles.scss"
 import "./about.scss"
-import {
-	FaBeer,
-	FaGraduationCap,
-	FaBookReader,
-	FaLightbulb,
-	FaAtom,
-	FaHandshake
-} from "react-icons/fa"
-
 export default function About() {
 	return (
 		<section className="section" id="about">
@@ -24,7 +17,8 @@ export default function About() {
 							<div className="box">
 								<article className="media">
 									<div className="media-left">
-										<FontAwesomeIcon icon={faCoffee} />
+										{/* <FontAwesomeIcon icon={faCoffee} /> */}
+										<img src={coffee_icon} alt="" srcset="" />
 									</div>
 									<div className="media-content">
 										<div className="content">
@@ -45,7 +39,8 @@ export default function About() {
 							<div className="box">
 								<article className="media">
 									<div className="media-left">
-										<FaGraduationCap size="1em" className="fa-fw" />
+										{/* <FaGraduationCap size="1em" className="fa-fw" /> */}
+										<img src={handshake_icon} alt="" srcset="" />
 									</div>
 									<div className="media-content">
 										<div className="content">
@@ -54,8 +49,8 @@ export default function About() {
 											</h3>
 											<p className="is-size-5 is-size-6-mobile">
 												chcemy aby dzięki takim inicjatywom, uczniowie
-												warszawskich szkół wpływali na otoczenie dookoła siebie i
-												zmieniali swoje szkoły na lepsze.
+												warszawskich szkół wpływali na otoczenie dookoła siebie
+												i zmieniali swoje szkoły na lepsze.
 											</p>
 										</div>
 									</div>
@@ -66,11 +61,13 @@ export default function About() {
 							<div className="box">
 								<article className="media">
 									<div className={`media-left fa-fw`}>
-										<FaHandshake className="fa-fw" />
+										<img src={friendship_icon} alt="" srcset=""/>
 									</div>
 									<div className="media-content">
 										<div className="content">
-											<h3 className="title is-3 is-size-4-mobile">Lorem ipsum</h3>
+											<h3 className="title is-3 is-size-4-mobile">
+												Lorem ipsum
+											</h3>
 											<p className="is-size-5 is-size-6-mobile">
 												jednym z powodów dla których zapraszamy was na Wiśniową,
 												jest chęć umożliwienia nawiązania nowych wartościowych
@@ -85,11 +82,14 @@ export default function About() {
 							<div className="box">
 								<article className="media">
 									<div className={`media-left fa-fw`}>
-										<FaLightbulb size="1em" className="fa-fw" />
+										{/* <FaLightbulb size="1em" className="fa-fw" /> */}
+										<img src={lightbulb_icon} alt="" srcset="" />
 									</div>
 									<div className="media-content">
 										<div className="content">
-											<h3 className="title is-3 is-size-4-mobile">Lorem ipsum</h3>
+											<h3 className="title is-3 is-size-4-mobile">
+												Lorem ipsum
+											</h3>
 											<p className="is-size-5 is-size-6-mobile">
 												W gronie aktywnych i kreatywnych łatwo wpada się na nowe
 												pomysły oraz znajduje motywacje oraz partnerów do ich
