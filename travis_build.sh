e63b2b50000974847af55b6460dbbf1ca1c5117e
Piotr Stadnicki <pitstadnicki@gmail.com>
2020-01-21 21:03:35 +0100

    Added listing

--- travis_build.sh
+++ travis_build.sh
@@ -4,7 +4,9 @@ echo $TRAVIS_BRANCH
 echo "========================="
 if [[ $TRAVIS_BRANCH == "master" ]] || [[ $TRAVIS_BRANCH == "development" ]]
 then
-    echo "Sending files..."
+    echo "Listing files:"
+    ls -la
+    echo "Packing files..."
     cd public/ && tar -zcvf ../build.tar.gz . && cd - 
     echo "Sending files..."
     jo key=$KEY branch=$TRAVIS_BRANCH build=%build.tar.gz | curl -H "Content-Type:application/json" -X POST -d @- $HOOK
