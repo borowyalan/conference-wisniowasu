b2787eed4e91f985e57ee30205bc5c805824481a
Piotr Stadnicki <pitstadnicki@gmail.com>
2020-01-26 15:06:09 +0100

    Fixed gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
 	siteMetadata: {
-		title: "Konferencja Samorządów Warszawskich | Wiśniowa SU",
+		title: `Konferencja Samorządów Warszawskich | Wiśniowa SU`,
 		description: 'Sprawdź co możemy zrobic wspólnymi siłamy na konferencji samorządów na Wiśniowej',
-		author: "@Borowy Alan",
+		author: `@Borowy Alan`,
 	},
 	plugins: [
 		{
-			resolve: "gatsby-plugin-google-analytics",
+			resolve: `gatsby-plugin-google-analytics`,
 			options: {
 			  // The property ID; the tracking code won't be generated without it
 			  trackingId: "UA-156094275-2",
-			  // Defines where to place the tracking script - "true" in the head and "false" in the body
-			  head: false,
+			  // Defines where to place the tracking script - `true` in the head and `false` in the body
+			  head: true,
 			  // Setting this parameter is optional
 			  anonymize: true,
 			  // Setting this parameter is also optional
@@ -26,31 +26,31 @@ module.exports = {
 			  cookieDomain: "pasje.wisniowasu.pl",
 			},
 		},
-		"gatsby-plugin-sass",
-		"gatsby-plugin-react-helmet",
+		`gatsby-plugin-sass`,
+		`gatsby-plugin-react-helmet`,
 		{
-			resolve: "gatsby-source-filesystem",
+			resolve: `gatsby-source-filesystem`,
 			options: {
-				name: "images",
-				path: "${__dirname}/src/images",
+				name: `images`,
+				path: `${__dirname}/src/images`,
 			},
 		},
-		"gatsby-transformer-sharp",
-		"gatsby-plugin-sharp",
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
 		{
-			resolve: "gatsby-plugin-manifest",
+			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: "gatsby-starter-default",
-				short_name: "starter",
-				start_url: "/",
-				background_color: "#663399",
-				theme_color: "#663399",
-				display: "minimal-ui",
-				icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
 			},
 		},
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
-		// "gatsby-plugin-offline",
+		// `gatsby-plugin-offline`,
 	],
 }
